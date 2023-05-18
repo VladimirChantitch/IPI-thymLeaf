a780547001ee82bd7352056acb8ded2199095660
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-05-18 11:37:54 +0200

    Ajoutez des fichiers projet.

---
+++ main/java/com/ipi/jva320/controller/HomeController.java
@@ -0,0 +1,55 @@
+package com.ipi.jva320.controller;
+
+import com.ipi.jva320.exception.SalarieException;
+import com.ipi.jva320.model.SalarieAideADomicile;
+import com.ipi.jva320.service.SalarieAideADomicileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private SalarieAideADomicileService salarieAideADomicileService;
+
+
+    @GetMapping(value = "/")
+    public String home(ModelMap model) {
+        model.put("salarieCount", salarieAideADomicileService.countSalaries());
+        return "home";
+    }
+
+    @GetMapping(value = "/salaries/{id}")
+    public String salarie(ModelMap model, @PathVariable Long id) {
+        model.put("salarie", salarieAideADomicileService.getSalarie(id));
+        return "detail_Salarie";
+    }
+
+    @GetMapping(value = "/salaries/aide/new")
+    public String newSalarie(ModelMap model) {
+        return "detail_Salarie";
+    }
+
+    @PostMapping(value = "/salaries/save")
+    public String createSalarie(SalarieAideADomicile salarie) throws SalarieException {
+        salarieAideADomicileService.creerSalarieAideADomicile(salarie);
+        return "redirect:/salaries/" + salarie.getId();
+    }
+
+    @PostMapping(value = "/salaries/{id}")
+    public String updateSalarie(SalarieAideADomicile salarie) throws SalarieException {
+        salarieAideADomicileService.updateSalarieAideADomicile(salarie);
+        return "redirect:/salaries/" + salarie.getId();
+    }
+
+    @GetMapping(value = "/salaries")
+    public String getSalaries(ModelMap model) {
+        model.put("salaries", salarieAideADomicileService.getSalaries());
+        return "list";
+    }
+
+}
---
+++ main/java/com/ipi/jva320/service/DataInitService.java
@@ -0,0 +1,30 @@
+package com.ipi.jva320.service;
+
+import com.ipi.jva320.model.SalarieAideADomicile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+/**
+ * Ajoute des données de test si vide au démarrage
+ */
+@Component
+public class DataInitService implements CommandLineRunner {
+
+    @Autowired
+    private SalarieAideADomicileService salarieAideADomicileService;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        if (this.salarieAideADomicileService.countSalaries() != 0) {
+            return;
+        }
+
+        SalarieAideADomicile s1 = this.salarieAideADomicileService.creerSalarieAideADomicile(
+                new SalarieAideADomicile("Jean", LocalDate.now(), LocalDate.now(),
+                20, 0,
+                80, 5, 1));
+    }
+}
